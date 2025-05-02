5c86caf0964fb53e701c7dea14a0cb7643f4cdae
Aravindh1505 <aravindh.samidurai@gmail.com>
2025-05-02 11:34:45 +0800

    Updated the theme and removed enableEdgeToEdge method

--- app/src/main/java/com/aravindh/composeplayground/MainActivity.kt
+++ app/src/main/java/com/aravindh/composeplayground/MainActivity.kt
@@ -3,7 +3,6 @@ package com.aravindh.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import com.aravindh.composeplayground.ui.theme.ComposePlaygroundTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposePlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
